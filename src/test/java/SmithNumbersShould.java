04208ec86eac34c50c4f844814639e32d518f9ca
alvaro garcia+marc manzano <derecerca+manzanomarc@gmail.com>
2015-09-09 19:06:28 +0200

    DRY: simplified two prime tests

--- src/test/java/SmithNumbersShould.java
+++ src/test/java/SmithNumbersShould.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,26 +20,21 @@ public class SmithNumbersShould {
     }
 
     @Test
-    public void detect_the_smallest_smith_number(){
-        assertThat(isSmith(2), is(true));
-    }
-
-    @Test
     public void detect_the_smallest_non_smith_number_except_the_corner_cases() {
         assertThat(isSmith(6), is(false)); // DC(6)=2*3; S=5
     }
 
     @Test
-    public void detect_the_first_smith_number_after_the_smallest_non_smith_number() {
+    public void detect_prime_number_as_smith() {
+        assertThat(isSmith(2), is(true));
         assertThat(isSmith(7), is(true)); // DC(7)=7; S=7
     }
 
-
     private static boolean isSmith(int n) {
-        final List<Integer> primes = Arrays.asList(7);
+        final List<Integer> primes = Arrays.asList(2,7);
         if(primes.contains(n)) {
             return true;
         }
-        return n > 1 && n < 6;
+        return false;
     }
 }
