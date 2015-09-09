cddcadc1216226a12ce89c5022c06744fdb5cb05
alvaro garcia+marc manzano <derecerca+manzanomarc@gmail.com>
2015-09-09 18:44:51 +0200

    defect: zero cannot be decomposed into primes, therefore, is not a smith number

--- src/test/java/SmithNumbersShould.java
+++ src/test/java/SmithNumbersShould.java
@@ -6,11 +6,11 @@ import static org.hamcrest.Matchers.is;
 public class SmithNumbersShould {
 
     @Test
-    public void detect_the_smallest_smith_number() {
-        assertThat(isSmith(0), is(true));
+    public void detect_the_smallest_non_smith_number() {
+        assertThat(isSmith(0), is(false));
     }
 
     private static boolean isSmith(int n) {
-        return true;
+        return false;
     }
 }
