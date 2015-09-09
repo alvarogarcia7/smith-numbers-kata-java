edd41d8f7020e7934ac81d7da14fe807647bd45a
alvaro garcia+marc manzano <derecerca+manzanomarc@gmail.com>
2015-09-09 18:36:43 +0200

    red: detect the first smith number

---
+++ src/test/java/SmithNumbersShould.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SmithNumbersShould {
+
+    @Test
+    public static void detect_the_smallest_smith_number() {
+        assertThat(isSmith(0), is(true));
+    }
+
+    private static boolean isSmith(int n) {
+        return false;
+    }
+}
