676b79d5c5c79515e403a9cecb13210563d97e60
alvaro garcia+marc manzano <derecerca+manzanomarc@gmail.com>
2015-09-09 19:36:55 +0200

    red: test all smith numbers from OEIS

--- src/test/java/SmithNumbersShould.java
+++ src/test/java/SmithNumbersShould.java
@@ -30,6 +30,19 @@ public class SmithNumbersShould {
         assertThat(isSmith(7), is(true)); // DC(7)=7; S=7
     }
 
+    @Test
+    public void detect_smith_numbers_from_oeis() {
+        areSmith(4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526,
+                535, 562, 576, 588, 627, 634, 636, 645, 648, 654, 663, 666, 690, 706, 728, 729, 762, 778, 825, 852, 861,
+                895, 913, 915, 922, 958, 985, 1086, 1111, 116);
+    }
+
+    private void areSmith(int... candidates) {
+        for (int current : candidates) {
+            assertThat(isSmith(current), is(true));
+        }
+    }
+
     private static boolean isSmith(int n) {
         final List<Integer> primes = Arrays.asList(2,7);
         if(primes.contains(n)) {
